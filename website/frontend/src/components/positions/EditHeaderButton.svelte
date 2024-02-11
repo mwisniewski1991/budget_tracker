80cc6162055b3ab6e1f57d2fcdfbae92c719e1d0
mwisniewski <mw.data.web@gmail.com>
2024-02-11 20:53:02 +0100

    Code refactor

---
+++ website/frontend/src/components/positions/EditHeaderButton.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let headerIDtoEdit;
+
+    let isEdit = false;
+
+    async function editHeader(headerID, val){
+    console.log(headerID);         
+        isEdit = val;
+        // const parameters = new URLSearchParams({headerid: headerID});
+        // const resposne = await fetch(`/api/v1/position-delete?${parameters}`, {method:"DELETE"})
+        // const results =  await resposne.json();
+        // location.reload()
+    };
+
+
+</script>
+
+{#if isEdit}
+<button type="button" class="btn btn-info" on:click={editHeader(headerIDtoEdit, false)}>ENTER</button>            
+{:else}
+<button type="button" class="btn btn-info" on:click={editHeader(headerIDtoEdit, true)}>EDIT</button>            
+{/if}
