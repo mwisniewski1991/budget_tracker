9b47c0528edce98598dc41d4571049ce839f7230
mwisniewski <mw.data.web@gmail.com>
2024-03-07 22:08:55 +0100

    Change variables process. Remove unnecessery files.

--- website/app/__init__.py
+++ website/app/__init__.py
@@ -1,15 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from os import path, environ
+from os import environ
 from dotenv import load_dotenv
 
 
 db = SQLAlchemy()
 
 def create_app():
-    basedir = path.abspath(path.dirname(__file__))
-    load_dotenv(path.join(basedir, ".env"))
+    # basedir = path.abspath(path.dirname(__file__))
+    # load_dotenv(path.join(basedir, ".env"))
 
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('SQLALCHEMY_DATABASE_URI')
