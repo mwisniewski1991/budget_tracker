1d92c86ba40b5ed76be00bdca0561b0f70df179f
mwisniewski <mw.data.web@gmail.com>
2024-04-19 21:17:25 +0200

    Change endpoint to delete all INCEXP.

--- website/frontend/src/components/IncExp/existed/components/buttons/DeleteHeaderButton.svelte
+++ website/frontend/src/components/IncExp/existed/components/buttons/DeleteHeaderButton.svelte
@@ -2,9 +2,7 @@
     export let headerIDtoDelete;
 
     async function deleteHeader(headerID){
-        const parameters = new URLSearchParams({headerid: headerID});
-        const resposne = await fetch(`/api/v1/position-delete?${parameters}`, {method:"DELETE"})
-        const results =  await resposne.json();
+        const resposne = await fetch(`/api/v1/position-delete/${headerID}`, {method:"DELETE"})
         location.reload()
     };
 
