a8425581023633b32dfdad8f764c5565dc443b8d
mwisniewski <mw.data.web@gmail.com>
2024-06-06 22:09:51 +0200

    Add docstrings

--- website/app/blueprints/incexp/utils.py
+++ website/app/blueprints/incexp/utils.py
@@ -1,5 +1,18 @@
 def category_subcategory_decrypt(value: str) -> list:
+    '''Function decrypt category_id (e.g. '01') and subcategory_id (e.g. '0001') from format '01_0001'. 
+
+    :param value: string for decrypting
+    :returns: list[str, str]
+
+    '''
     return value.split('_')
 
 def category_subcategory_encrypt(value_one: str, value_second:str) -> list:
+    '''Function encrypt category_id (e.g. '01') and subcategory_id (e.g. '0001') into format '01_0001'. 
+
+    :param value_one: category_id from models
+    :param value_second: subcategory_id from models
+    :returns: str
+    
+    '''
     return f'{value_one}_{value_second}'
