b54bc30aabb388426f183001d277a42715b152db
mwisniewski <mw.data.web@gmail.com>
2024-06-15 12:49:49 +0200

    Add fucntion for more complex query

--- website/app/blueprints/incexp/utils.py
+++ website/app/blueprints/incexp/utils.py
@@ -1,3 +1,6 @@
+from ...models import INCEXP_header, INCEXP_position
+from sqlalchemy import and_
+
 def master_slave_decrypt(value: str) -> list:
     '''Function decrypt master value (e.g. '01') and slave value (e.g. '0001') from format '01_0001'. 
     Functions use for 
@@ -20,3 +23,48 @@ def master_slave_encrypt(master_value: str, slave_value:str) -> list:
     :returns: str
     '''
     return f'{master_value}_{slave_value}'
+
+
+def incexp_query_existings(
+                    results_limit:int,
+                    owner_id:int, 
+                    account_id:str, 
+                    type_id:int = None,
+                    created_date_from:str = None,
+                    created_date_to:str = None,
+                    source:str = None,
+                    comment:str = None,
+                    connection:str = None,
+                ) -> list:
+                    
+    incexp_list = (INCEXP_header
+            .query
+            .filter(and_(INCEXP_header.owner_id==owner_id, INCEXP_header.account_id==account_id))
+            )
+
+    if type_id:
+        incexp_list = incexp_list.filter(INCEXP_header.type_id == type_id)
+
+    if created_date_from:
+        incexp_list = incexp_list.filter(INCEXP_header.date >= created_date_from)
+
+    if created_date_to:
+        incexp_list = incexp_list.filter(INCEXP_header.date <= created_date_to)
+    
+    if source:
+        incexp_list = incexp_list.filter(INCEXP_header.source.ilike(f'%{source}%'))
+
+    if created_date_from:
+        incexp_list = incexp_list.filter(INCEXP_header.date >= created_date_from)
+
+    if comment:
+        incexp_list = incexp_list.filter(INCEXP_header.incexp_positions.any(INCEXP_position.comment.ilike(f"%{comment}%")))
+
+    if connection:
+        incexp_list = incexp_list.filter(INCEXP_header.incexp_positions.any(INCEXP_position.connection.ilike(f"%{connection}%")))
+
+    return (incexp_list
+                    .order_by(INCEXP_header.date.desc(), INCEXP_header.id)
+                    .limit(results_limit)
+                   ).all()
+
