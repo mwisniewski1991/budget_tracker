9ff5aac6a57013e4160ca3d1131518960b82ba9d
mwisniewski <mw.data.web@gmail.com>
2024-06-15 12:50:31 +0200

    Add incexp_query_existings function into routes

--- website/app/blueprints/incexp/incexp.py
+++ website/app/blueprints/incexp/incexp.py
@@ -3,7 +3,7 @@ from sqlalchemy import text, and_, select, func, case, alias
 from ... import db
 from ...models import Owners, INCEXP_header, INCEXP_position, Category, Subategory
 from .forms import Incexp_header_form
-from .utils import master_slave_decrypt, master_slave_encrypt
+from .utils import master_slave_decrypt, master_slave_encrypt, incexp_query_existings
 import logging
 
 DEFAULT_OWNER_ACCOUNT_IDS = '1_05'
@@ -29,38 +29,18 @@ def get_incexp():
     comment = request.args.get('comment', None)
     connection = request.args.get('connection', None)
 
-    incexp_list = (INCEXP_header
-                .query
-                .filter(and_(INCEXP_header.owner_id==owner_id, INCEXP_header.account_id==account_id))
-                )
-    
-    if type_id:
-        incexp_list = incexp_list.filter(INCEXP_header.type_id == type_id)
+    incexp_list = incexp_query_existings(
+        results_limit,
+        owner_id,
+        account_id,
+        type_id,
+        created_date_from,
+        created_date_to,
+        source,
+        comment,
+        connection,
+    )
 
-
-    if created_date_from:
-        incexp_list = incexp_list.filter(INCEXP_header.date >= created_date_from)
-
-    if created_date_to:
-        incexp_list = incexp_list.filter(INCEXP_header.date <= created_date_to)
-    
-    if source:
-        incexp_list = incexp_list.filter(INCEXP_header.source.ilike(f'%{source}%'))
-
-    if created_date_from:
-        incexp_list = incexp_list.filter(INCEXP_header.date >= created_date_from)
-
-    if comment:
-        incexp_list = incexp_list.filter(INCEXP_header.incexp_positions.any(INCEXP_position.comment.ilike(f"%{comment}%")))
-
-    if connection:
-        incexp_list = incexp_list.filter(INCEXP_header.incexp_positions.any(INCEXP_position.connection.ilike(f"%{connection}%")))
-
-
-    incexp_list = (incexp_list
-                    .order_by(INCEXP_header.date.desc(), INCEXP_header.id)
-                    .limit(results_limit)
-                   ).all()
 
     owners = Owners.query.order_by(Owners.id).all()
 
@@ -77,7 +57,6 @@ def get_incexp():
                             incexp_header_form=incexp_header_form, 
                             owners=owners,
                             owner_account_ids=owner_account_ids)
-
 
 @incexp.route('/', methods=['POST'])
 def add_incexp():
@@ -112,16 +91,9 @@ def add_incexp():
                 db.session.add(new_incexp_position)
         db.session.commit()
 
-
     results_limit = request.args.get('limit', 50)
 
-    incexp_list = (INCEXP_header
-                .query
-                .filter(and_(INCEXP_header.owner_id==owner_id, INCEXP_header.account_id==account_id))
-                .order_by(INCEXP_header.date.desc(), INCEXP_header.id)
-                .limit(results_limit)
-                ).all()
-    
+    incexp_list = incexp_query_existings(results_limit, owner_id, account_id)
 
     return render_template('incexp/incexp_list_existing.html.jinja', incexp_list=incexp_list)
 
