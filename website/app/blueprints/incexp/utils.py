18c7b1e06b9506e295320ec5c2f3589aef8d76a2
mwisniewski <mw.data.web@gmail.com>
2024-06-15 12:29:50 +0200

    Change name and refactor fucntions.

--- website/app/blueprints/incexp/utils.py
+++ website/app/blueprints/incexp/utils.py
@@ -1,18 +1,22 @@
-def category_subcategory_decrypt(value: str) -> list:
-    '''Function decrypt category_id (e.g. '01') and subcategory_id (e.g. '0001') from format '01_0001'. 
+def master_slave_decrypt(value: str) -> list:
+    '''Function decrypt master value (e.g. '01') and slave value (e.g. '0001') from format '01_0001'. 
+    Functions use for 
+        - category and subcategory (e.g. '01_0001')
+        - owners and accounts (e.g. '1_01')
 
     :param value: string for decrypting
     :returns: list[str, str]
-
     '''
     return value.split('_')
 
-def category_subcategory_encrypt(value_one: str, value_second:str) -> list:
-    '''Function encrypt category_id (e.g. '01') and subcategory_id (e.g. '0001') into format '01_0001'. 
+def master_slave_encrypt(master_value: str, slave_value:str) -> list:
+    '''Function encrypt master value (e.g. '01') and slave value (e.g. '0001') into format '01_0001'. 
+    Functions use for 
+        - category and subcategory (e.g. '01_0001')
+        - owners and accounts (e.g. '1_01')
 
     :param value_one: category_id from models
     :param value_second: subcategory_id from models
     :returns: str
-    
     '''
-    return f'{value_one}_{value_second}'
+    return f'{master_value}_{slave_value}'
