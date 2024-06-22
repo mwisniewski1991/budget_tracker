e1371aa343ea3f2a691d9d1117e4d2f298384d7f
mwisniewski <mw.data.web@gmail.com>
2024-06-22 22:26:47 +0200

    Register incomes-categories blueprint

--- website/app/__init__.py
+++ website/app/__init__.py
@@ -12,6 +12,11 @@ def create_app():
     app = Flask(__name__)
     app.config.from_prefixed_env()
 
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://budget_user:KY]bU)km+}VMV#4,fm@192.168.0.142:5432/budget_tracker'
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://mw:mw@192.168.0.101:5432/budget_tracker_test'
+    # app.config['SECRET_KEY'] = '123_456_789'
+    # app.config['DEFAULT_RESULTS_LIMIT'] = 50
+
     db.init_app(app)
     migrate = Migrate(app, db)
 
@@ -25,10 +30,13 @@ def create_app():
     from .blueprints.accounts_results.accounts_results import accounts_results
     from .blueprints.owners.owners import owners
     from .blueprints.incexp.incexp import incexp
+    from .blueprints.incomes_categories.incomes_categories import incomes_categories
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(accounts_results, url_prefix='/accounts-results')
     app.register_blueprint(owners, url_prefix='/owners')
     app.register_blueprint(incexp, url_prefix='/incexp')
+    app.register_blueprint(incomes_categories, url_prefix='/incomes-categories')
+
 
     return app
