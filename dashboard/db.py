90f4f2d2c293bb3e685fb81dffbbde135992732e
mwisniewski1991 <mw.data.web@gmail.com>
2026-02-15 17:46:34 +0100

    Dashborad: change database connection function

--- dashboard/db.py
+++ dashboard/db.py
@@ -12,7 +12,13 @@ QUERIES_DIR = Path(__file__).parent / "queries"
 
 @st.cache_resource
 def get_connection():
-    return psycopg2.connect(environ["FLASK_SQLALCHEMY_DATABASE_URI"])
+    return psycopg2.connect(
+        host=environ["DB_HOST"],
+        port=environ["DB_PORT"],
+        dbname=environ["DB_NAME"],
+        user=environ["DB_USER"],
+        password=environ["DB_PASSWORD"],
+    )
 
 
 def run_query(sql_file: str, params: dict) -> pd.DataFrame:
